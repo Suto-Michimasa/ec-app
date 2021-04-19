4759ec96b0d79e477e3e791929b353be805a48e9
Suto <sutou1335michimasa@gmail.com>
2021-04-19 10:56:34 +0900

    ファイルの整理

---
+++ src/templates/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TextInput } from "../components/UIkit"
+
+const SignUp = () => {
+  return (
+    <div className="c-section-container">
+      <h2 className="u-text_headline u-text-center">アカウント登録</h2>
+      <div className="module-space-medium"></div>
+      <TextInput
+
+      />
+
+    </div>
+  )
+}
+
+export default SignUp
