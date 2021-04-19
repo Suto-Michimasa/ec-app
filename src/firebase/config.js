72970f37b49fedf5f594231482ad60ff7367fc77
Suto <sutou1335michimasa@gmail.com>
2021-04-19 10:29:12 +0900

    firebaseの導入

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_PhzLvUBJ6LDUclBV7FozA32keHEjqU4",
+  authDomain: "ec-app-ed99b.firebaseapp.com",
+  projectId: "ec-app-ed99b",
+  storageBucket: "ec-app-ed99b.appspot.com",
+  messagingSenderId: "71057559879",
+  appId: "1:71057559879:web:b3bbdb369430a0e4e93c62",
+  measurementId: "G-1KKLNX4BV2"
+};
