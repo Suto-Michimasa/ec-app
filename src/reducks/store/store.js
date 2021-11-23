57bce311ca0e492b3b34a2b74570c146a90b47aa
Suto <sutou1335michimasa@gmail.com>
2021-11-23 17:37:58 +0900

    build product redux components

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -6,10 +6,12 @@ import {
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import { UsersReducer } from "../users/reducers";
 import thunk from 'redux-thunk'
+import { ProductsReducer } from "../products/reducers";
 
 export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
+      products: ProductsReducer,
       router: connectRouter(history),
       users: UsersReducer
     }),
